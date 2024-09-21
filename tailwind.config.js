abd2bb1cbd26a795ecf0a7f1b90b02fd1acb908b
Kevin Reyes <kerpa9@gmail.com>
2024-09-21 16:56:02 -0500

    Desing header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        principal: ["Playfair Display", "serif"],
+      },
+
+      colors: {
+        "bright-red": "hsl(12, 88%, 59%)",
+        "dark-blue": "hsl(228, 39%, 23%)",
+      },
+
+      backgroundImage: {
+        "close-menu": "url('../../assets/images/icon-close.svg')",
+        "open-menu": "url('../../assets/images/icon-hamburger.svg')",
+      },
+    },
   },
   plugins: [],
 };
